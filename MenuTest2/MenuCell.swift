1eb2cdf304b0ead93300317d62d626c8e16b223e
jisung <jisung.smart@gmail.com>
2020-01-16 15:24:45 +0900

    Add Delegate Method

---
+++ MenuTest2/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MenuTest2
+//
+//  Created by Hae kyeom Kim on 2020/01/16.
+//  Copyright © 2020 Hae kyeom Kim. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
--- MenuTest2/ViewController.swift
+++ MenuTest2/ViewController.swift
@@ -30,8 +30,66 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         selectedArray = contriesArray[selectedIndex]
+        menuCollection.selectItem(at: selectedIndexPath, animated: true, scrollPosition: .centeredVertically)
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction))
+        leftSwipe.direction = .left
+        self.view.addGestureRecognizer(leftSwipe)
+        
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction))
+        rightSwipe.direction = .right
+        self.view.addGestureRecognizer(rightSwipe)
+        
+        indicatorView.backgroundColor = .white
+        indicatorView.frame  = CGRect(x: menuCollection.bounds.minX, y: menuCollection.bounds.maxY - indicatorHeight, width: menuCollection.bounds.width, height: indicatorHeight)
+        menuCollection.addSubview(indicatorView)
+        
+    }
+    
+    @objc func swipeAction(_ sender: UISwipeGestureRecognizer){
+        if sender.direction == .left{
+            if selectedIndex < menuTitles.count - 1 {
+                selectedIndex += 1
+            }
+        } else {
+            if selectedIndex > 0 {
+                selectedIndex -= 1
+            }
+        }
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = selectedArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuTitles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
